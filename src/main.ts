6c4ca556c6d5e7b3c2a989e52cfec11efee80f20
Joel Kesler <jkesler@membersolutions.com>
2025-11-25 16:06:08 -0600

    switch to zoneless app

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
-import { provideZoneChangeDetection } from "@angular/core";
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { appConfig } from './app/app.config';
 
-bootstrapApplication(AppComponent, {...appConfig, providers: [provideZoneChangeDetection(), ...appConfig.providers]}).catch((err) =>
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err),
 );
